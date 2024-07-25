e1b48e3bfa4be879dc395cf01b2fad78d8d9edc9
youngha123 <dudgk7829@naver.com>
2024-07-25 12:44:34 +0900

    git commit -m 20240709 git commit -m

--- officetel_example.sql
+++ officetel_example.sql
@@ -1,17 +1,17 @@
--- 데이터베이스 생성 
+-- 데이터베이스 생성
 -- officetel
 CREATE DATABASE officetel;
 USE officetel;
 
 -- 이메일 인증 테이블
 CREATE TABLE email_auth (
-	email VARCHAR(100) PRIMARY KEY CHECK(email REGEXP '^[a-zA-Z0-9]*@([-.]?[a-zA-Z0-9])*\.[a-zA-Z]{2,4}$'),
+    email VARCHAR(100) PRIMARY KEY CHECK(email REGEXP '^[a-zA-Z0-9]*@([-.]?[a-zA-Z0-9])*\.[a-zA-Z]{2,4}$'),
     auth_number VARCHAR(4) NOT NULL CHECK(auth_number REGEXP '^[0-9]{4}$')
 );
 
--- 회원 테이블 
+-- 회원 테이블
 CREATE TABLE user (
-	id VARCHAR(20) PRIMARY KEY,
+    id VARCHAR(20) PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     join_path VARCHAR(5) NOT NULL DEFAULT 'HOME',
@@ -21,53 +21,53 @@ CREATE TABLE user (
 
 -- 게시물 테이블
 CREATE TABLE board (
-	receipt_number INT PRIMARY KEY AUTO_INCREMENT, 
+    receipt_number INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(50) NOT NULL,
     status BOOLEAN NOT NULL DEFAULT false,
     writer_id VARCHAR(20) NOT NULL,
     write_date DATETIME NOT NULL DEFAULT now(),
     view_count INT NOT NULL DEFAULT 0,
-	contents VARCHAR(1000) NOT NULL,
-	reply TEXT NOT NULL,
-	CONSTRAINT writer_fk FOREIGN KEY (writer_id) REFERENCES user(id)
+    contents VARCHAR(1000) NOT NULL,
+    reply TEXT,
+    CONSTRAINT writer_fk FOREIGN KEY (writer_id) REFERENCES user (id)
 );
 
 -- 로그인 
 SELECT * FROM user 
-WHERE id = 'qwer1234' AND password = 'qwer1234';
+WHERE id = :id AND password = :password;
 
-SELECT * FROM userWHERE id = :id; 
+SELECT * FROM user WHERE id = :id;
 
 -- 아이디 중복확인
-SELECT * FROM user WHERE id = 'qwer1234';
+SELECT * FROM user WHERE id = :id;
 
 -- 이메일 인증
-INSERT INTO email_auth VALUES ('qwer1234@qwer.com', '0123');
+INSERT INTO email_auth VALUES (:email, :auth_number);
 
 -- 이메일 인증 확인
 SELECT * FROM email_auth 
-WHERE email = 'qwer1234@qwer.com' AND auth_number = '0123';
+WHERE email = :email AND auth_number = :auth_number;
 
 -- 회원가입
-INSERT INTO user(id, password, email) VALUES ('qwer1234', 'qwer1234', 'qwer1234@qwer.com');
+INSERT INTO user(id, password, email) VALUES (:id, :password, :email);
 
 -- 게시물 리스트
 SELECT receipt_number, status, title, writer_id, write_date, view_count
 FROM board
-WHERE title LIKE '%%'
+WHERE title LIKE '%두%'
 ORDER BY receipt_number DESC
 LIMIT 0, 10;
 
 -- 게시물 작성
 INSERT INTO board (title, contents, writer_id)
-VALUES ('첫번째 게시물', '안녕하세요', 'qwer1234');
+VALUES ('열두번째 게시물', '안녕하세요', 'qwer1234');
 
 -- 게시물 상세보기
 UPDATE board SET view_count = view_count + 1
 WHERE receipt_number = 1;
 
 SELECT 
-	receipt_number,
+    receipt_number,
     title,
     writer_id,
     write_date,
@@ -86,14 +86,4 @@ UPDATE board SET title = '게시물 수정', contents = '안녕하세요'
 WHERE receipt_number = 1;
 
 -- 게시물 삭제
-DELETE FORM board WHERE receipt_number = :receipt_number;
-
-
-
-
-
-
-
-
-
-
+DELETE FROM board WHERE receipt_number = 1;
